6bfe870fd895e168a1ac05adc578104c5a0e031e
Antti Kivimäki <antti.kivimaki@futurice.com>
2017-12-27 21:40:54 +0200

    Don't use nfc service if built for web

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -6,9 +6,11 @@ import App from './App';
 import router from './router';
 import store from './store';
 import http from './services/http';
-import './services/nfc';
 
-if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
+if (!process.env.IS_WEB) {
+  require('./services/nfc');
+  Vue.use(require('vue-electron'));
+}
 
 Vue.http = Vue.prototype.$http = http;
 
