d2790453feec231d7ef443c9f563a78a8a57629f
Antti Kivimäki <antti.kivimaki@futurice.com>
2019-01-05 00:37:30 +0200

    Give navigation bar a new look

--- src/renderer/components/Navigation.vue
+++ src/renderer/components/Navigation.vue
@@ -7,7 +7,7 @@
     </div>
     <hr class="splitter" />
     <router-link class="link" v-for="link in links" :to="{ name: link.name }" :key="link.label">
-      <div class="link-wrapper" >
+      <div>
         <i :class="['fa', `fa-${link.icon}`]" aria-hidden="true"/>
         <div>
           <span>{{ link.label }}</span>
@@ -23,9 +23,9 @@
     data() {
       return {
         links: [
-          { label: 'Me', icon: 'user-circle-o', name: 'user-page' },
-          { label: 'Group', icon: 'users', name: 'group-page' },
-          { label: 'Settings', icon: 'cogs', name: 'settings-page' },
+          { label: 'ME', icon: 'user-circle-o', name: 'user-page' },
+          { label: 'GROUP', icon: 'users', name: 'group-page' },
+          { label: 'SETTINGS', icon: 'cogs', name: 'settings-page' },
         ],
       };
     },
@@ -34,32 +34,31 @@
 
 <style lang="scss" scoped>
   .navigation {
-    background: #fbfbfb;
+    background: #4b29c5;
     position: fixed;
     top: 0;
     left: 0;
     text-align: center;
     height: 100vh;
-    box-shadow: 0 0 15px #ccc;
   }
 
   .logo {
     padding-top: 10px;
 
     img {
-      width: 70%;
+      width: 80%;
     }
   }
 
   .splitter {
-    width: 90%;
-    border-top: 1px solid #c1c1c1;
+    width: 100%;
+    border-top: 1px solid white;
     margin-bottom: 0;
   }
 
   .link {
-    border-bottom: 1px solid #c1c1c1;
-    font-size: 1.2vw;
+    font-size: 1.4vw;
+    width: 100%;
     cursor: pointer;
 
     & > div {
@@ -67,29 +66,27 @@
     }
 
     span {
-      color: black;
+      font-weight: bold;
+      color: white;
     }
 
     i.fa {
-      color: #225fc6;
+      color: white;
       font-size: 4vw;
+      margin-bottom: 2px;
     }
 
     &:hover > div {
-      background: #cccccc;
+      background: #a293d6;
     }
 
     &.router-link-active {
-      span {
-        color: white;
-      }
-
-      i.fa {
-        color: white;
+      span, i.fa {
+        color: #4b29c5;
       }
 
       & > div {
-        background: #225fc6;
+        background: white;
       }
     }
   }
